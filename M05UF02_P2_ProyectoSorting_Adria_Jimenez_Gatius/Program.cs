d8f7d8502adea4e84e26a37ee266c10db00f8a62
AdriaJiGa <ADRIAJIGA@GMAIL.COM>
2022-05-01 18:44:02 +0200

    prueba bucket fallida

--- M05UF02_P2_ProyectoSorting_Adria_Jimenez_Gatius/Program.cs
+++ M05UF02_P2_ProyectoSorting_Adria_Jimenez_Gatius/Program.cs
@@ -262,32 +262,95 @@ namespace M05UF02_P2_ProyectoSorting_Adria_Jimenez_Gatius
             }
         }
 
-        // Driver code
-        public static void Main()
+
+
+
+
+public class HeapSort
+    {
+        public void sort(int[] arr)
         {
-            float[] arr = { (float)0.897, (float)0.565,
-                   (float)0.656, (float)0.1234,
-                   (float)0.665, (float)0.3434 };
-
             int n = arr.Length;
-            bucketSort(arr, n);
 
-            Console.WriteLine("Sorted array is ");
-            foreach (float el in arr)
+            // Build heap (rearrange array)
+            for (int i = n / 2 - 1; i >= 0; i--)
+                heapify(arr, n, i);
+
+            // One by one extract an element from heap
+            for (int i = n - 1; i > 0; i--)
             {
-                Console.Write(el + " ");
+                // Move current root to end
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                // call max heapify on the reduced heap
+                heapify(arr, i, 0);
             }
         }
 
+        // To heapify a subtree rooted with node i which is
+        // an index in arr[]. n is size of heap
+        void heapify(int[] arr, int n, int i)
+        {
+            int largest = i; // Initialize largest as root
+            int l = 2 * i + 1; // left = 2*i + 1
+            int r = 2 * i + 2; // right = 2*i + 2
 
+            // If left child is larger than root
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
 
+            // If right child is larger than largest so far
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
 
+            // If largest is not root
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
 
+                // Recursively heapify the affected sub-tree
+                heapify(arr, n, largest);
+            }
+        }
+
+        /* A utility function to print array of size n */
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.Read();
+        }
+
+        // Driver code
+        public static void Main()
+        {
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            int n = arr.Length;
+
+            HeapSort ob = new HeapSort();
+            ob.sort(arr);
+
+            Console.WriteLine("Sorted array is");
+            printArray(arr);
+        }
     }
 
+ 
 
 
-    class Program
+
+
+
+}
+
+
+
+class Program
     {
         static void Main(string[] args)
         {
@@ -306,7 +369,9 @@ namespace M05UF02_P2_ProyectoSorting_Adria_Jimenez_Gatius
             array.Sort(array.InsertionSort);
             //array.Sort(array.MergeSort);
             //Prueba2
-            array.
+            
+          
+            
 
         }
     }
